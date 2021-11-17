69e6a6fcca903ff6b6b59db7e05e3c1cc4f5c4d5
Rasmus Halvgaard <raha@computas.com>
2021-11-17 10:28:36 +0100

    Add sudo

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 # Build and deploy predictor API
-docker build -f Dockerfile_api -t nace-predictor:latest .
-docker run -d -p 5000:5000 nace-predictor:latest
+sudo docker build -f Dockerfile_api -t nace-predictor:latest .
+sudo docker run -d -p 5000:5000 nace-predictor:latest
 
 # Check that the API service runs
 curl localhost:5000/_ah/warmup
 
 # Build and deploy web API
-docker build -f Dockerfile_client -t nace-client:latest .
-docker run -d -p 3000:3000 nace-client:latest
+sudo docker build -f Dockerfile_client -t nace-client:latest .
+sudo docker run -d -p 3000:3000 nace-client:latest
 
 # Go to website
 http://localhost:3000
